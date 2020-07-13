3a4a5caeb51e6517fd905ae0f59234c096d59ef6
AHMED AFANDY <eng.computer1@yahoo.com>
2020-07-13 00:06:36 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+
+
+  end
+
+  def artist_name
+    "#{self.artist.name}"
+  end
+  def artist_name_f
+       "#{self.artist.name} - #{self.title}"
+  end
 end
